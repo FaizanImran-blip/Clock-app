94af1886856c80782628145053784ecb24168dfc
M Faizan <f2024376210@umt.edu.pk>
2025-05-07 22:41:44 -0700

    new

--- new-clock.dart
+++ new-clock.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'dart:async';
 
-class Clock {
+class Cloc {
   late Timer _timer;
   final int h = 20;
   final int w = 29;
@@ -12,7 +12,7 @@ class Clock {
   }
 
   void dis(int hours, int minutes, int seconds) {
-    print("=========================::2D-Clock::=====================");
+    print("=========================::2D-Cloc::=====================");
     for (int i = 0; i < h; i++) {
       if (i == h ~/ 2 - 1) {
         stdout.write("||" + "\t" * 3 + "Hours   : ${hours.toString().padLeft(2, '0')}" + "\t" * 3 + "||\n");
@@ -44,12 +44,12 @@ class Clock {
 }
 
 void main() {
-  Clock c = Clock();
+  Cloc c = Cloc();
   print("Press Enter to continue...");
   stdin.readLineSync();
 
   print("1 :- Check exact date and time");
-  print("2 :- Start the Clock");
+  print("2 :- Start the Cloc");
   int ch = int.parse(stdin.readLineSync()!);
 
   switch (ch) {
